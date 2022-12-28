757dcba522ae6c1116f2af88c5c17305d832ccaf
Shorok Atwa <72991188+shorokatwa14@users.noreply.github.com>
2022-12-28 14:34:57 +0200

    Add files via upload

---
+++ js/codejs.js
@@ -0,0 +1,17 @@
+function clicklinkedin() {
+    window.open("https://www.linkedin.com/in/shorok-m-atwa-279a991b6/", "_blank");
+}
+function clickgithub() {
+    window.open("https://github.com/shorokatwa14", "_blank");
+}
+function clicktelegram() {
+    window.open("https://t.me/Shorokatwa14", "_blank");
+}
+function clickemail() {
+    window.open("mailto:sheko147atwa@gmail.com", "_blank");
+}
+function clickfacebook() {
+    window.open("https://www.facebook.com/shorok_atwa", "_blank");
+}
+
+
